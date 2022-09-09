f25d92eb87b8d276dfda47b75ec7fc1042eb86a1
Kieran Wales <17WalesK@fulford.york.sch.uk>
2022-09-09 15:02:03 +0100

    Added comments

--- MainProject/Program.cs
+++ MainProject/Program.cs
@@ -7,13 +7,17 @@ using System.Threading.Tasks;
 namespace MainProject
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
+            // create a variable to store the integer value of the number of pizzas
             int numPizzas;
             Console.WriteLine("How many pizzas would you like to eat?");
+
+            // take the user input for how many pizzas they would like
             numPizzas = int.Parse(Console.ReadLine());
 
+            // create a loop so that each console message has them eat a different pizza up to their chosen amount
             for (int i = 0; i < numPizzas; i++)
             {
                 Console.WriteLine("Eat pizza number " + (i + 1));
