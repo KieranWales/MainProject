677ec83338523658758e9a0a2d847cbd92f1d5dc
17WalesK <17WalesK@fulford.york.sch.uk>
2022-09-09 14:43:50 +0100

    Add project files.

---
+++ MainProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
+            Console.WriteLine("\nPress enter to exit");
+            Console.ReadLine();
+        }
+    }
+}
