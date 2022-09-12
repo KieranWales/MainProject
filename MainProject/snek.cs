80c73caffe934850401acdcef4f7251d86b62cc8
Kieran Wales <17WalesK@fulford.york.sch.uk>
2022-09-12 15:09:43 +0100

    Created board

--- MainProject/snek.cs
+++ MainProject/snek.cs
@@ -8,9 +8,28 @@ namespace MainProject
 {
     class snek
     {
+        // map = [9, 9];
         static void Main(string[] args)
         {
-            Console.WriteLine("hi");
+            string snake = "";
+            int[] pos = { 5, 5 };
+
+            while (pos[0] < 9 && pos[1]> 0)
+            {
+                for (int a = 0; a < 20; a++)
+                {
+                    Console.WriteLine("\n");
+                }
+
+                Console.WriteLine("#######################################################################################################################");
+                for(int i = 0; i < 27; i++)
+                {
+                    Console.WriteLine("#                                                                                                                     #");
+                }
+                Console.WriteLine("#######################################################################################################################");
+
+                System.Threading.Thread.Sleep(1000);
+            }
         }
     }
 }
