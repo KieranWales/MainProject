6b5736546ffae54ad36252c3b57ff7734d0de90c
Kieran Wales <17WalesK@fulford.york.sch.uk>
2022-09-23 15:19:13 +0100

    Added snake

--- MainProject/snek.cs
+++ MainProject/snek.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace MainProject
 {
@@ -10,23 +11,22 @@ namespace MainProject
     {
         // map = [9, 9];
         static string direction = "right";
+        static string snake = "";
+        static int[] pos = { 5, 5 };
         static System.Timers.Timer overallTimer = new System.Timers.Timer();
+        static List<int[]> oldPos = new List<int[]>();
 
         static void Main(string[] args)
         {
-            overallTimer.Interval = 1000;
+            overallTimer.Interval = 100;
             overallTimer.Enabled = true;
-
-            string snake = "";
-            int[] pos = { 5, 5 };
+            overallTimer.Elapsed += Update;
+            overallTimer.Elapsed += Screen;
 
             Setup();
 
-            while (pos[0] < 9 && pos[1] > 0)
-            {
-                Screen();
-                Update(pos, direction);
-            }
+            while (pos[0] < Console.WindowWidth && pos[0] > 0 && pos[1] < Console.WindowHeight && pos[1] > 0){}
+            overallTimer.Enabled = false;
         }
 
         static void Setup()
@@ -41,14 +41,11 @@ namespace MainProject
             Console.CursorVisible = false;
         }
             
-        static void Screen()
+        static void Screen(object source, ElapsedEventArgs e)
         {
-            if (Console.WindowWidth != 120 || Console.WindowHeight != 30)
-            {
-                Console.WindowWidth = 120;
-                Console.WindowHeight = 30;
-                Console.SetBufferSize(120, 30);
-            }
+            Console.WindowWidth = 120;
+            Console.WindowHeight = 30;
+            Console.SetBufferSize(120, 30);
         }
 
         static void gameplay()
@@ -56,11 +53,24 @@ namespace MainProject
             Console.ReadLine();
         }
 
-        static void Update(int[] pos, string direction)
+        static void Update(object source, ElapsedEventArgs e)
         {
+            while (oldPos.Count() > snake.Length)
+            {
+                Console.SetCursorPosition(oldPos[0][0], oldPos[0][1]);
+                Console.Write(" ");
+                oldPos.RemoveAt(0);
+            }
+
+            foreach (int[] position in oldPos)
+            {
+                Console.SetCursorPosition(position[0], position[1]);
+                Console.Write("0");
+            }
+
             Console.SetCursorPosition(pos[0], pos[1]);
             Console.Write("0");
-            string.Concat(Enumerable.Repeat("", 2));
+            oldPos.Add(new int[] {pos[0], pos[1]});
 
             if (direction == "up")
             {
@@ -78,6 +88,11 @@ namespace MainProject
             {
                 pos[0] += 1;
             }
+        }
+
+        static void GetInput()
+        {
+
         }
     }
 }
